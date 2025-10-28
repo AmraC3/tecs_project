10f6fff296a4388114b3b2649da17abcd1b4d099
AmraC3 <calashouse03@gmail.com>
2025-10-28 13:12:10 +0100

    style added

--- tecs_app/static/tecs_app/js/script.js
+++ tecs_app/static/tecs_app/js/script.js
@@ -1,1 +1,17 @@
-console.log("Homepage del Teatro caricata correttamente!");
+document.addEventListener("DOMContentLoaded", () => {
+  const accordions = document.querySelectorAll(".accordion-header");
+
+  accordions.forEach(header => {
+    header.addEventListener("click", () => {
+      const item = header.parentElement;
+
+      // Chiude eventuali altri accordion aperti
+      document.querySelectorAll(".accordion-item").forEach(i => {
+        if (i !== item) i.classList.remove("active");
+      });
+
+      // Alterna quello cliccato
+      item.classList.toggle("active");
+    });
+  });
+});
--- tecs_project/settings.py
+++ tecs_project/settings.py
@@ -106,6 +106,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    BASE_DIR / 'tecs_app' / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
